97e3d10d497cb93774584865861dce32249bf54f
Daniel <daniel@statusengine.org>
2015-06-28 23:27:41 +0200

    Refactoring

--- install-monitoring-plugins.sh
+++ install-monitoring-plugins.sh
@@ -3,8 +3,8 @@ set -e
 wget https://github.com/monitoring-plugins/monitoring-plugins/archive/v2.1.1.tar.gz
 tar xfv v2.1.1.tar.gz
 cd monitoring-plugins-2.1.1/
-./autogen.sh
-./configure --prefix=/opt/naemon
+./autogen.sh --prefix=/opt/naemon
+#./configure --prefix=/opt/naemon
 make all
 make install
 cd ..
--- install-naemon.sh
+++ install-naemon.sh
@@ -5,8 +5,8 @@ wget https://github.com/naemon/naemon-core/archive/v1.0.3.tar.gz
 tar xfv v1.0.3.tar.gz
 cd naemon-core-1.0.3/
 mkdir -p /opt/naemon
-./autogen.sh
-./configure --prefix=/opt/naemon --with-naemon-user=naemon --with-naemon-group=www-data
+./autogen.sh --prefix=/opt/naemon --with-naemon-user=naemon --with-naemon-group=www-data
+#./configure --prefix=/opt/naemon --with-naemon-user=naemon --with-naemon-group=www-data
 make all
 make install
 make install-init
