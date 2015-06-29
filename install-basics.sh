92906efdf6726db3e250d3b7b0c02da863a169ab
Daniel <daniel@statusengine.org>
2015-06-29 12:29:30 +0200

    Add install-basics file

--- install-basics.sh
+++ install-basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-sudo apt-get install -y tmux htop vim nginx php5-fpm gearman-job-server libgearman-dev gearman-tools uuid-dev php5-gearman php5-cli php5-dev libjson-c-dev manpages-dev build-essential help2man gperf install rrdtool php5-rrd
+sudo apt-get install -y tmux htop vim nginx php5-fpm gearman-job-server libgearman-dev gearman-tools uuid-dev php5-gearman php5-cli php5-dev libjson-c-dev manpages-dev build-essential help2man gperf rrdtool php5-rrd
 
