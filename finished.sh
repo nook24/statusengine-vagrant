1195015a9653834f0f28981a843eed03ef3ac7af
Daniel <daniel@statusengine.org>
2015-07-01 14:48:20 +0200

    Add auto ip discovery

--- finished.sh
+++ finished.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+#Thanks to: http://askubuntu.com/questions/560412/displaying-ip-address-on-eth0-interface
+ip=`ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1`
+
 echo "--------------------------"
 echo -e "Setup done"
 echo "MySQL Username: root Passwort: vagrant"
 echo "SSH: ssh vagrant@127.0.0.1 -p 2222 Passwort: vagrant"
-echo "Open a browser an navigate to localhost:8080 to visit Statusengine Interface"
+echo "Statusengine interface user: admin password: admin"
+echo "Open your browser and navigate to $ip/statusengine to visit Statusengine Interface"
+echo "If the ip is not working you can use localhost:8080/statusengine"
 echo "--------------------------"
 echo "Nice to know vagrant commands:"
 echo "vagrant ssh      - Open a shell via ssh"
