22c7705e65fa2c7e272baaf84beb5745a9401e82
nook24 <info@nook24.eu>
2015-07-13 17:06:31 +0200

    Overwrite default apache2 index page

--- install-basics.sh
+++ install-basics.sh
@@ -2,3 +2,6 @@
 set -e
 apt-get install -y apache2 libapache2-mod-php5 tmux htop vim gearman-job-server libgearman-dev gearman-tools uuid-dev php5-gearman php5-cli php5-dev libjson-c-dev manpages-dev build-essential help2man gperf rrdtool php5-rrd php5-mysqlnd unzip
 
+#Overwrite default apache index
+cp var/www/html/index.html /var/www/html/index.html
+
