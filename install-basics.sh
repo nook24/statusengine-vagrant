bd1dcb81f66f6a03bdaee4b2e811bb7d10aa2024
nook24 <info@nook24.eu>
2015-07-13 19:31:07 +0200

    Add libtool to apt-get

--- install-basics.sh
+++ install-basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-apt-get install -y apache2 libapache2-mod-php5 tmux htop vim gearman-job-server libgearman-dev gearman-tools uuid-dev php5-gearman php5-cli php5-dev libjson-c-dev manpages-dev build-essential help2man gperf rrdtool php5-rrd php5-mysqlnd unzip
+apt-get install -y apache2 libapache2-mod-php5 tmux htop vim gearman-job-server libgearman-dev gearman-tools uuid-dev php5-gearman php5-cli php5-dev libjson-c-dev manpages-dev build-essential help2man gperf libtool rrdtool php5-rrd php5-mysqlnd unzip
 
