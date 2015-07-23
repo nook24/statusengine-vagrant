9900c0cb8d85eea6c30189aa27c476d0d6f54ec7
Daniel <nook24@users.noreply.github.com>
2015-07-23 21:15:28 +0200

    wget --no-verbose

--- install-monitoring-plugins.sh
+++ install-monitoring-plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #Download compile and install monitoring plugins
-wget https://github.com/monitoring-plugins/monitoring-plugins/archive/v2.1.1.tar.gz
+wget https://github.com/monitoring-plugins/monitoring-plugins/archive/v2.1.1.tar.gz --no-verbose
 tar xfv v2.1.1.tar.gz
 cd monitoring-plugins-2.1.1/
 ./autogen.sh --prefix=/opt/naemon
