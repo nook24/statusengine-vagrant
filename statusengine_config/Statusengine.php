8da28bad71e23953bf60e5ad2ab52d48d5a9ce95
Daniel <nook24@users.noreply.github.com>
2015-09-03 20:13:45 +0200

    Update Statusengine.php

--- statusengine_config/Statusengine.php
+++ statusengine_config/Statusengine.php
@@ -7,7 +7,7 @@
 
 $config = [
 	//Version of Statusengine
-	'version' => '1.5.0',
+	'version' => '1.5.2',
 	
 	//Logfile, where statusengine will log some information
 	'logfile' => '/var/log/statusengine.log',
